f4880d92400cfad7b52b0b5e446c160dad257893
Firas <f.sahli@quict.nl>
2021-02-20 15:05:47 +0100

    create model for article, view, rating
    Signed-off-by: Firas <f.sahli@quict.nl>

---
+++ app/Models/Article.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'desc'
+    ];
+
+    public function views(){
+      return $this->hasMany(Views::class);
+    }
+
+    public function ratings(){
+      return $this->hasMany(Rating::class);
+    }
+
+    public function categories() {
+      return $this->belongsToMany(Category::class, 'category_article');
+    }
+}
