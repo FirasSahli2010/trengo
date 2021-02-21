3da6409b8d1e6efa61b70ed3205c33364fd3d2fb
Firas <f.sahli@quict.nl>
2021-02-21 18:11:26 +0100

    update api routes to reflect current functionalities
    Signed-off-by: Firas <f.sahli@quict.nl>

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('article/category/{category_list}', [ArticleController::class, 'category_search']);
+// Route::post('article',  'App\Http\Controllers\ArticleController@create');
+//
+// Route::get('article', [ArticleController::class, 'index']);
+
+Route::apiResource('article', ArticleController::class);
+Route::apiResource('category', CategoryController::class);
